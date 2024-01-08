a55de952ba5000edb47880f00c0f2e27156aaf80
xhaeffer <subhansyahsp@gmail.com>
2024-01-09 00:25:56 +0700

    re set ffmpeg, improve code syntax, convert cmd typo with regex, implement error handler for quoted stiker cmd, add 'jpg/png/gif/mp4 to sticker via document' upload cmd (gif mp4 still broken)

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const ffmpeg = require('ffmpeg-static');
-const cloudConvert = require('cloudconvert');
-const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMjM4YjJjM2VkNjhiZTZkN2ZiYWUwZWEyYzRiYzdjMDI2ZmQzMzFiYTI2ZjUzNTM2OWM3MTM2NTY4MDhmYTNiMDJlZWU5ZmRiNDM5NzA4NGMiLCJpYXQiOjE3MDQzODg0MzYuMjg0NTgzLCJuYmYiOjE3MDQzODg0MzYuMjg0NTg1LCJleHAiOjQ4NjAwNjIwMzYuMjgwMTc2LCJzdWIiOiI2NjcwOTkzMyIsInNjb3BlcyI6WyJ1c2VyLnJlYWQiLCJ1c2VyLndyaXRlIiwidGFzay5yZWFkIiwidGFzay53cml0ZSIsIndlYmhvb2sucmVhZCIsIndlYmhvb2sud3JpdGUiLCJwcmVzZXQucmVhZCIsInByZXNldC53cml0ZSJdfQ.C0-k0YRorxby0uRIldrLe_i1ycWhs-T31zg6f7O7QXzbp7SOQUdF-O7vWTLJj9LH1RkiIQv3WGL3ByU0F94rmUUu7jcfr6jBmJfIL9T9rqzbVPlyYfLGCjleDddXgAOh6rk7uSpcuxt2Ya4mjEfrnp-WxprysPEv-UHuhI9Uq1S_tOBenW7_R1QsBnKHrOn9H5pjBrAyl3VVzDH4KrSZUvwT8p6Fth7c9Fx_44FaFU0dQWwhvmViV2gPfeZvRCYCE36k4MJremXZR0NSaKK9POTuiio5bXa5aHSo8GAUhM0jTZHzKYTN9gwxhYT9uMLXnOajcvFRowQJnWcFJnsrO94HxaajXfH-8vy81XFatHm-1Hmmm-QdmjRuXVI6-Cc594oLw9fQDXrsWWb1QKPagMvgYNxnuRrpcyDR9a3ePElCgjABegJAAu3MCqn-I0yflNrIRKKNfm9-DZI8HjY9NfhlutpqKgJURb5OW6QlCt7ig6GrFVtmwrKpYWHAxBWUsOutCaFZwu7x4k7OWzfJEqXrC8m974ic-EyfsGDio3miOWtNC6t9R3JsnRjqBDeBHmo9GMdi86umagIJ-J1yzatGAE8vo1k3nlD3nM4xpPLmns8NJnyY698BW13qB0AUJmYnTqeE6CPQD3VYi0wYkkKuYnO6sZeU1qHfcXbYGCM';
-var convertapi = require('convertapi')('c9CU3XiuRSAMfYAx');
-const fs = require('fs');
-const { exec } = require('child_process');
+
+const fs = require("fs");
+const sharp = require("sharp");
+
+const ffmpegInstaller = require("@ffmpeg-installer/ffmpeg");
+const ffprobe = require("@ffprobe-installer/ffprobe");
+const ffmpeg = require("fluent-ffmpeg")()
+.setFfprobePath(ffprobe.path)
+.setFfmpegPath(ffmpegInstaller.path);
+
+const osPlatform = require("os").platform(); // possible values are: 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+console.log("Scraper running on platform: ", osPlatform);
+let execPath;
+if (/^win/i.test(osPlatform)) {
+  execPath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+} else if (/^linux/i.test(osPlatform)) {
+  execPath = "/usr/bin/chromium-browser";
+}
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  ffmpegPath: ffmpeg,
-  puppeteer: {executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',}
+  ffmpegPath: ffmpegInstaller.path,
+  puppeteer: {
+    executablePath: execPath,
+  },
 });
 
 const messageQueue = [];
 let isProcessingQueue = false;
 
-const timeer = 5000;
+const timeer = 10000;
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
@@ -30,7 +44,6 @@ client.on("message", async (message) => {
   try {
     const body = message.body.toLowerCase();
 
-    // Tambahkan semua keyword ke dalam fungsi processMessageQueue()
     messageQueue.push(message);
 
     if (!isProcessingQueue) {
@@ -50,158 +63,195 @@ async function processMessageQueue() {
   while (messageQueue.length > 0) {
     const message = messageQueue.shift();
     const body = message.body.toLowerCase();
-    let quotedMessage = ""
+
+    const regexTitik = /^\.(?!(stiker|foto|gif))/i;
+    const regexSeru = /^!(stiker|foto|gif)/i;
+    const regexPlus = /^\+(stiker|foto|gif)/i;
+    const regexKeywordOnly = /^(stiker|foto|gif|kurni|stiiker|stijer|sticker)$/i;
+    const isInvalidCommand = regexTitik.test(body) || regexSeru.test(body) || regexPlus.test(body) || regexKeywordOnly.test(body);
+
 
     switch (true) {
+      case isInvalidCommand:
+        await delay(timeer);
+        message.reply(
+          "Ngetiknya yang bener 😠\nketik *.help* for more info"
+        );
+        break; 
+
       case ["p", "hi"].includes(body):
         await delay(timeer);
         message.reply(
-          "Kamu nyari aku 😨\nKalo mau buat stiker perintahnya .stiker disertai dengan gambar/video ya 😁"
+          "Kamu nyari aku 😨\nKalo mau buat stiker perintahnya *.stiker + gambar/video/gif* ya 😁"
         );
         break;
 
-      case [".help", "!help"].includes(body):
+      case [".help", "!help", "+help"].includes(body):
         await delay(timeer);
         message.reply(
           "*Note:*\n" +
-          "Setiap perintah mempunyai *delay ± 10detik*,\n" +
-          "harap *bersabar* dan *jangan sampai spam*\n\n" +
-          "*Daftar Perintah:*\n" +
-          " # Buat Sticker (Img, Vid, Gif): \n - .stiker\n\n" +
-          " # Stiker ke Foto & Gif: \n - .foto | reply stikernya pake .foto _(beta)_ \n - .gif _(coming soon)_ \n\n" +
-          " # Tes bot:\n - p\n - hi \n\n" +
-          " # Bantuan:\n - .help\n\n" +
-          "_Bot masih dalam tahap pengembangan, dan untuk perintah unik lainnya coming soon~_"
+            "Setiap perintah mempunyai *delay ± 10detik*,\n" +
+            "harap *bersabar* dan *jangan sampai spam*\n\n" +
+            "*Fitur:*\n" +
+            " *🟩 Buat Sticker (IMG, VID, GIF):*\n" +
+            "  *🔸Cara pakai:* \n" +
+            "      - _`masukkan media + perintah`_. \n" +
+            "      - _`reply media + perintah`_. \n" +
+            "  *🔸Perintah:* \n" +
+            "      - .stiker\n\n" +
+            " *|❗Beta❗|*\n" +
+            " *🟩 Konversi Stiker ke Foto & Gif:*\n" +
+            "  *🔸Cara pakai:* \n" +
+            "      _`reply stiker + masukin perintah`_. \n" +
+            "  *🔸Perintah:* \n" +
+            "      - .foto \n" +
+            "      - .gif (_*new!*_)\n\n" +
+            " *🟩 Tes Bot:*\n" +
+            "  *🔸Perintah:* \n" +
+            "      - p\n" +
+            "      - hi\n\n" +
+            " *🟩 Bantuan:*\n" +
+            "  *🔸Perintah:* \n" +
+            "      - .help\n\n" +
+            "_Bot masih dalam tahap pengembangan, dan untuk perintah unik lainnya coming soon~_"
         );
         break;
 
-      case [".kurni", "kurni", ".sticker", ". stiker", ". sticker", "stiker", "stiiker", "sticker", "!stiker", "! stiker", "!sticker", "! sticker"].includes(body):
-        await delay(timeer);
-        message.reply(
-          "Ngetiknya yang bener 😠\nketik *.stiker + masukin gambar/video* untuk mulai membuat stiker"
-        );
+      case [".stiker"].includes(body) && !message.hasQuotedMsg:
+        if (["image", "video", "gif"].includes(message.type)) {
+          await delay(timeer);
+          await processStickerMessage(message);
+        }
+        else if (["document"].includes(message.type)) {
+          if (["image/jpg", "image/png", "image/gif", "video/mp4"].includes(message._data.mimetype)) {
+            await delay(timeer);
+            await processStickerMessage(message);
+          }
+          else {
+            await delay(timeer);
+            message.reply("Cuma bisa Gambar, Video dan GIF 👍");
+          }
+        }
+        else if (["chat"].includes(message.type)) {
+          await delay(timeer);
+          message.reply("Eitss, gambar/videonya ketinggalan nih..\n*ketik .stiker + masukin gambar/video* untuk mulai membuat stiker");
+        }
+        else {
+          await delay(timeer);
+          message.reply("Cuma bisa Gambar, Video dan GIF 👍");
+        }
+
         break;
 
-      case [".stiker"].includes(body) && ["image", "video", "gif"].includes(message.type):
-        await processStickerMessage(message);
-        break;
+      case [".stiker"].includes(body) && message.hasQuotedMsg:
+        const quotedMessageStiker = await message.getQuotedMessage();
 
-      case [".stiker"].includes(body) && ["audio", "voice", "document", "location", "text"].includes(message.type):
-        await delay(timeer);
-        message.reply("Cuma bisa Gambar, Video dan GIF 👍");
-        break;
-
-      case [".stiker"].includes(body):
-        await delay(timeer);
-        message.reply(
-          "Eitss, gambar/videonya ketinggalan nih..\n*ketik .stiker + masukin gambar/video* untuk mulai membuat stiker"
-        );
+        if (quotedMessageStiker && ["image", "video", "gif"].includes(quotedMessageStiker.type)) {
+          await delay(timeer);
+          await processStickerMessage(quotedMessageStiker);
+        }
+        else {
+          await delay(timeer);
+          message.reply("Cuma bisa Gambar, Video dan GIF 👍");
+        }
         break;
 
       case [".foto"].includes(body) && message.hasQuotedMsg:
         await delay(timeer);
-        message.reply("Memproses stiker menjadi foto...");
-        await delay(timeer);
-        quotedMessage = await message.getQuotedMessage();
-        if (quotedMessage.type === "sticker") {
-          const stickerMedia = await quotedMessage.downloadMedia();
+        if (message.type === "sticker" && !message.body);
+        message.reply("Memproses ubah stiker menjadi IMG...");
+
+        const quotedMessageImg = await message.getQuotedMessage();
+        if (quotedMessageImg && quotedMessageImg.type === "sticker") {
+          const stickerMedia = await quotedMessageImg.downloadMedia();
+          await delay(timeer);
           message.reply(stickerMedia, undefined, {
-            caption: "Oke",
+            caption: "Oke 👍👌",
           });
         }
-        await delay(timeer);
         break;
 
       case [".gif"].includes(body) && message.hasQuotedMsg:
         await delay(timeer);
-        message.reply("Mengubah stiker menjadi gif...");
-        quotedMessage = await message.getQuotedMessage();
-        if (quotedMessage.type === "sticker") {
-          const stickerMedia = await quotedMessage.downloadMedia();
-          const buffer = stickerMedia.data;
-          
-          async function webpToGif() {
-            const cloudConvertInstance = new cloudConvert(apiKey);
-            try {
-                let exampleJob = await cloudConvertInstance.jobs.create({
-                    "tasks": {
-                        "import-my-file": {
-                            "operation": "import/base64",
-                            "file": buffer,
-                            "filename": "input.webp"
-                        },
-                        "convert-my-file": {
-                            "operation": "convert",
-                            "input": "import-my-file",
-                            "output_format": "gif"
-                        },
-                        "export-my-file": {
-                            "operation": "export/url",
-                            "input": "convert-my-file"
-                        }
-                    }
-                });
-                let job = await cloudConvertInstance.jobs.wait(exampleJob.id);
-                const file = cloudConvertInstance.jobs.getExportUrls(job)[0];
-                return file.url
-        
-            } catch (error) {
-                console.error('Error:', error);
-            }
-          }
-
-          const gif = await webpToGif()
-
-          async function gifToMp4() {
-            const cloudConvertInstance = new cloudConvert(apiKey);
-            try {
-                let exampleJob = await cloudConvertInstance.jobs.create({
-                    "tasks": {
-                        "import-my-file": {
-                            "operation": "import/url",
-                            "url": gif
-                        },
-                        "convert-my-file": {
-                            "operation": "convert",
-                            "input": "import-my-file",
-                            "output_format": "mp4"
-                        },
-                        "export-my-file": {
-                            "operation": "export/url",
-                            "input": "convert-my-file"
-                        }
-                    }
-                });
-                let job = await cloudConvertInstance.jobs.wait(exampleJob.id);
-                const file = cloudConvertInstance.jobs.getExportUrls(job)[0];
-                return file.url
-        
-            } catch (error) {
-                console.error('Error:', error);
-            }
-          }
-          const mp4 = await gifToMp4()
-
-          const media = await MessageMedia.fromUrl(mp4);
-
-          message.reply(media, undefined, {
-            caption: "Inilah gif dari stiker!",
-            sendVideoAsGif: true,
-          });
-        } else {
-          message.reply("Stiker yang di-reply bukan stiker gif.");
-        }
+        if (message.type === "sticker" && !message.body);
+        message.reply("Memproses ubah stiker menjadi GIF...");
         await delay(timeer);
-        break;
+
+        const quotedMessage = await message.getQuotedMessage();
+
+        if (quotedMessage && quotedMessage.type === "sticker") {
+          const stickerMedia = await quotedMessage.downloadMedia();
+
+          if (stickerMedia) {
+            const gifBuffer = await convertWebpToGif(stickerMedia.data);
+            const gifMedia = new MessageMedia("gif", gifBuffer);
+
+            fs.writeFileSync(
+              "output.gif",
+              Buffer.from(gifMedia.data, "base64")
+            );
+
+            const gifPath = MessageMedia.fromFilePath("output.gif");
+
+
+
+            const ffmpegPromise = new Promise((resolve, reject) => {
+              ffmpeg
+                .input(`${gifPath.filename}`)
+                .outputOptions([
+                  "-pix_fmt yuv420p",
+                  "-c:v libx264",
+                  "-movflags +faststart",
+                  "-filter:v crop='floor(in_w/2)*2:floor(in_h/2)*2'",
+                ])
+                .noAudio()
+                .output(`vidgif.mp4`)
+                .on("end", () => {
+                  // console.log("Conversion to MP4 completed");
+                  resolve();
+                })
+                .on("error", (e) => {
+                  console.log("Error during conversion:", e);
+                  reject(e);
+                })
+                .run();
+            });
+
+            // Promise:
+            ffmpegPromise
+              .then(() => {
+                const mp4Media = MessageMedia.fromFilePath("vidgif.mp4");
+                message.reply(mp4Media, undefined, {
+                  caption: "Done 👍👌",
+                  sendVideoAsGif: true,
+                });
+
+                fs.unlinkSync("output.gif");
+                fs.unlinkSync("vidgif.mp4");
+              })
+              .catch((error) => {
+                console.error("Error during ffmpeg conversion:", error);
+                message.reply(
+                  "Terjadi kesalahan saat mengonversi stiker ke gif."
+                );
+              });
+          } else {
+            message.reply("Stiker yang di-reply bukan stiker gif.");
+          }
+          await delay(timeer);
+          break;
+        } else {
+          message.reply("Hanya berlaku untuk konversi STIKER ke GIF");
+          break;
+        }
 
       default:
         break;
     }
   }
 
-  if (messageQueue.length > 0) {
-    await processMessageQueue();
-  }
+  if (messageQueue.length > 0)
+    {await processMessageQueue();}
   isProcessingQueue = false;
 }
 
@@ -209,7 +259,7 @@ async function processStickerMessage(message) {
   const media = await message.downloadMedia();
 
   if (
-    (message.type === "video" || message.type === "gif") &&
+    (message.type === "video" || message.type === "gif" || message._data.mimetype === "image/gif" || message._data.mimetype === "video/mp4") &&
     message.duration
   ) {
     const videoDuration = parseFloat(message.duration);
@@ -230,6 +280,7 @@ async function processStickerMessage(message) {
     }
   }
 
+  await delay(timeer);
   message.reply("Memproses stiker...");
   await delay(timeer);
   message.reply(media, undefined, {
@@ -244,6 +295,23 @@ async function processStickerMessage(message) {
     await processMessageQueue();
   }
 }
+
+const convertWebpToGif = async (webpData) => {
+  try {
+    const buffer = Buffer.from(webpData, "base64");
+
+    const gifBuffer = await sharp(buffer, { animated: true })
+      .toFormat("gif")
+      .toBuffer();
+
+    const gifData = gifBuffer.toString("base64");
+
+    return gifData;
+  } catch (error) {
+    console.error("Error converting WebP to GIF:", error);
+    throw error;
+  }
+};
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
